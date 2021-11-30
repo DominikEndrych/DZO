1389f38e1a3f335217281c02312d05209ce866c1
Dominik <endrych.d@seznam.cz>
2021-11-30 15:44:47 +0100

    Projective transform
    Added Projective Transform functions. Also updated interpolation.h library so it is more readable.

--- cv1/DIP/DIP/interpolation.h
+++ cv1/DIP/DIP/interpolation.h
@@ -1,42 +1,37 @@
 #include "stdafx.h"
 
-static double getDec(double number)
+static double GetIntegral(double n)
 {
-	double _;
-	return modf(number, &_);
+	double fr;	// Only for modf(), does nothing
+	return modf(n, &fr);
 }
 
-template <typename T>
-T linearInterpolation(cv::Mat& img, double x, double y)
+template <typename T> T bilinearInterpolation(cv::Mat& image, double x, double y)
 {
-	return img.at<T>(std::floor(x), std::floor(y));
-}
+	cv::Point2d directions[4];
+	double coeficients[4];
 
-template <typename T>
-T bilinearInterpolation(cv::Mat& img, double x, double y)
-{
-	// ul, ur, ll, lr
-	cv::Point2d points[4] = {
-		cv::Point2d(std::floor(x), std::floor(y)),
-		cv::Point2d(std::floor(x), std::ceil(y)),
-		cv::Point2d(std::ceil(x), std::floor(y)),
-		cv::Point2d(std::ceil(x), std::ceil(y))
-	};
+	// Directions
+	directions[0] = cv::Point2d(std::floor(x), std::floor(y));
+	directions[1] = cv::Point2d(std::floor(x), std::ceil(y));
+	directions[2] = cv::Point2d(std::ceil(x), std::floor(y));
+	directions[3] = cv::Point2d(std::ceil(x), std::ceil(y));
 
-	double coefs[4] = {
-		(1 - getDec(x)) * (1 - getDec(y)),
-		(1 - getDec(x)) * (getDec(y)),
-		(getDec(x)) * (1 - getDec(y)),
-		(getDec(x)) * (getDec(y))
-	};
+	// Counting coeficients
+	coeficients[0] = (1 - GetIntegral(x)) * (1 - GetIntegral(y));
+	coeficients[1] = (1 - GetIntegral(x)) * (GetIntegral(y));
+	coeficients[2] = (GetIntegral(x)) * (1 - GetIntegral(y));
+	coeficients[3] = (GetIntegral(x)) * (GetIntegral(y));
 
-	T result;
+	T result;	// Result of interpolation
+
 	for (int i = 0; i < 4; i++)
 	{
-		cv::Point2d p = points[i];
-		if (p.x >= 0 && p.x < img.rows && p.y >= 0 && p.y < img.cols)
+		cv::Point2d p = directions[i];
+		if (p.x >= 0 && p.x < image.rows && 
+			p.y >= 0 && p.y < image.cols)
 		{
-			result += coefs[i] * img.at<T>(static_cast<int>(p.x), static_cast<int>(p.y));
+			result += coeficients[i] * image.at<T>((int)(p.x), (int)(p.y));
 		}
 	}
 
